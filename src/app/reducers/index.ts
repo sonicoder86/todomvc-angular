afd53daf97e599bd6d4ffc2ef0b3abf2bda1438b
blacksonic <soos.gabor86@gmail.com>
2020-02-12 22:07:13 +0100

    feat(core): upgrade to latest Angular 8

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,12 +1,6 @@
 import { Action, combineReducers } from '@ngrx/store';
 import { todoReducer } from './todo.reducer';
-import { State } from '../buzi';
 import { TodoInterface } from '../interfaces/todo.interface';
-
-// export function reducer(state: TodoInterface[] | undefined, action: Action) {
-//   return todoReducer(state, action);
-// }
-
 
 export function reducer(state: TodoInterface[] | undefined, action: Action) {
   return todoReducer(state, action);
