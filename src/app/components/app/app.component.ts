cf0a8936a3a16707b3c1355a354fdd6ac1a3f59c
blacksonic <soos.gabor86@gmail.com>
2020-02-15 19:33:52 +0100

    feat(core): make localstorage static

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { TodoStorageService } from '../../services/todo-storage.service';
+import { TodoLocalService } from '../../services/todo-local.service';
 import { Observable } from 'rxjs';
 import { TodoInterface } from '../../interfaces/todo.interface';
 import { TodoStateInterface } from '../../interfaces/todo-state.interface';
@@ -13,14 +13,14 @@ import { onLoad } from '../../state/actions/todo.actions';
 export class AppComponent implements OnInit  {
   todos$: Observable<TodoInterface[]>;
 
-  constructor(private store: Store<TodoStateInterface>, private storage: TodoStorageService) {
+  constructor(private store: Store<TodoStateInterface>) {
     this.todos$ = this.store.select('todos');
   }
 
   ngOnInit() {
     this.store.dispatch(onLoad(
-      this.storage.loadTodos()
+      TodoLocalService.loadTodos()
     ));
-    this.todos$.subscribe(todos => this.storage.storeTodos(todos));
+    this.todos$.subscribe(todos => TodoLocalService.storeTodos(todos));
   }
 }
--- src/app/services/todo-local.service.ts
+++ src/app/services/todo-local.service.ts
@@ -1,17 +1,15 @@
-import { Injectable } from '@angular/core';
 import { TodoInterface } from '../interfaces/todo.interface';
 
 const LOCAL_STORAGE_KEY = 'todoapp_todos';
 
-@Injectable({ providedIn: 'root' })
-export class TodoStorageService {
-  public loadTodos(): TodoInterface[] {
+export class TodoLocalService {
+  static loadTodos(): TodoInterface[] {
     return JSON.parse(
       window.localStorage.getItem(LOCAL_STORAGE_KEY) || '[]'
     );
   }
 
-  public storeTodos(todos: TodoInterface[]) {
+  static storeTodos(todos: TodoInterface[]) {
     window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }
 }
